a329621fd26897783941a4f5d99fcb3669cdb17c
Jordan Maulana <jordanmaulana26@gmail.com>
2019-03-20 08:41:40 +0700

    new

--- index.php
+++ index.php
@@ -33,7 +33,7 @@
 
     try {
         $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
-        $conn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch(Exception $e) {
         echo "Failed: " . $e;
     }
